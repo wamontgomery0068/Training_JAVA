7af13aa8758f5f7a4f3d2520bc49306ddcc37b7e
Your Name <wamontgomery0068@gmail.com>
2019-08-05 09:53:42 -0500

    datatype completed

---
+++ Basics/DataTypes/src/com/collabera/JAVA_Training/DataTypes.java
@@ -0,0 +1,124 @@
+package com.collabera.JAVA_Training;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+
+        System.out.println("Byte data type");
+        /*
+         * Whole numbers
+         * Byte data type
+         * 8 bits in size
+         * Minimal Value -128 (-2 ^ 7)
+         * Maximum Value 127 (2 ^ 7 - 1)
+         */
+        byte varByte = 9;
+
+        System.out.println(varByte);
+
+        System.out.println();
+
+        System.out.println("Short data type");
+
+        /*
+         * Whole numbers
+         * Short data type
+         * 16 bits in size
+         * Minimal Value -32,768 (-2 ^ 15)
+         * Maximum Value 32,767 (2 ^ 15 - 1)
+         */
+        short varShort = 10;
+
+        System.out.println(varShort);
+
+        System.out.println();
+
+        System.out.println("Integer data type");
+
+        /*
+         * Whole Numbers
+         * Integer data type
+         * 32 bits in size
+         * Minimal Value -2,147,483,648 (-2 ^ 31)
+         * Maximum Value 2,147,483,647 (2 ^ 31 - 1)
+         */
+        int varInt = 11;
+
+        System.out.println(varInt);
+
+        System.out.println();
+
+        System.out.println("Long data type");
+
+        /*
+         * Whole numbers
+         * Long data type
+         * 64 bits in size
+         * Minimal Value -9,223,372,036,854,775,808 (-2 ^ 63)
+         * Maximum Value 9,223,372,036,854,775,807 (2 ^ 63 - 1)
+         */
+        long varLong = 12;
+
+        System.out.println(varLong);
+
+        System.out.println();
+
+        System.out.println("Float data type");
+
+        // Decimal numbers
+        // float data type
+        // 32 bits in size
+
+        float varFloat = 13.0f;
+
+        System.out.println(varFloat);
+
+        System.out.println();
+
+        System.out.println("Double data type");
+
+        // Decimal numbers
+        // double data type
+        // 64 bits in size
+
+        double varDouble = 14.0d;
+
+        System.out.println(varDouble);
+
+        System.out.println();
+
+        System.out.println("Boolean data type");
+
+        // boolean data type
+        // true or false (1 or 0)
+        // 1 bit in size
+
+        boolean varBoolean = true;
+
+        System.out.println(varBoolean);
+
+        System.out.println();
+
+        System.out.println("Character data type");
+
+        // character data type
+        // 16 bit in size
+        // ASCII Table
+
+        char varCharacter = 'A';
+
+        System.out.println(varCharacter);
+
+        System.out.println();
+
+        System.out.println("String data type");
+
+        // string data type
+
+         String varString = "Hakuna Matata";
+
+        System.out.println(varString);
+    }
+}
