a20d25bff7906f37267a12717dee1deb979ebb41
Your Name <wamontgomery0068@gmail.com>
2019-08-05 10:08:11 -0500

    constant variables completed

---
+++ Basics/ConstantVariables/src/com/collabera/JAVA_Training/ConstantVariables.java
@@ -0,0 +1,20 @@
+package com.collabera.JAVA_Training;
+
+public class ConstantVariables {
+
+    public static void main(String[] args) {
+
+        // Constant:
+
+        // A constant variable means your variable remains a constant value as a result.
+        // A "Constant" is a variable whose value cannot change once it has been assigned.
+        // JAVA doesn't have a built-in support for constants, but the variable modifiers "static"
+        // and "final" can be used to effectively create one.
+
+         final float pi = 3.14159f;
+
+        System.out.println(pi);
+
+
+    }
+}
